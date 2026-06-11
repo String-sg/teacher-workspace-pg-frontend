64bc6487965aedf5b6c6a96b291e698b32d7c80b
Eileen Kang <eileen_kang@tech.gov.sg>
2026-06-11 20:43:28 +0800

    feat: add shadcn/ui foundation components

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,51 @@
+import { mergeProps } from '@base-ui/react/merge-props';
+import { useRender } from '@base-ui/react/use-render';
+import { cva } from 'class-variance-authority';
+import type { VariantProps } from 'class-variance-authority';
+
+import { cn } from '~/lib/utils';
+
+const badgeVariants = cva(
+  'h-5 gap-1 rounded-4xl border border-transparent px-2 py-0.5 text-xs font-medium transition-all has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&>svg]:size-3! inline-flex items-center justify-center w-fit whitespace-nowrap shrink-0 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-colors overflow-hidden group/badge',
+  {
+    variants: {
+      variant: {
+        secondary: 'border-border bg-secondary text-secondary-foreground [a]:hover:bg-secondary/80',
+        destructive:
+          'bg-destructive/10 [a]:hover:bg-destructive/20 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 text-destructive dark:bg-destructive/20',
+        success: 'bg-success text-success-foreground [a]:hover:bg-success/80',
+        warning: 'bg-warning text-warning-foreground [a]:hover:bg-warning/80',
+        info: 'bg-info text-info-foreground [a]:hover:bg-info/80',
+      },
+    },
+    defaultVariants: {
+      variant: 'info',
+    },
+  },
+);
+
+function Badge({
+  className,
+  variant = 'info',
+  render,
+  ...props
+}: useRender.ComponentProps<'span'> & VariantProps<typeof badgeVariants>) {
+  return useRender({
+    defaultTagName: 'span',
+    props: mergeProps<'span'>(
+      {
+        className: cn(badgeVariants({ className, variant })),
+      },
+      props,
+    ),
+    render,
+    state: {
+      slot: 'badge',
+      variant,
+    },
+  });
+}
+
+export type BadgeVariant = NonNullable<VariantProps<typeof badgeVariants>['variant']>;
+
+export { Badge, badgeVariants };
